110ddea5b43b704ce80299b139e0f77705bfc946
kousthubh <kyadavalli04@gmail.com>
2024-04-06 14:04:26 +0530

    feature successfully working

--- weather/weatherapp/views.py
+++ weather/weatherapp/views.py
@@ -1,21 +1,40 @@
 from django.shortcuts import render
 import requests
 import datetime 
+from django.http import JsonResponse
+
+# Function to fetch weather data from API
+def get_weather_data(city):
+    url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid=6a8c2720970c1dd78fb680438474bc0d'
+    params = {'units': 'metric'}
+    response = requests.get(url, params=params)
+    
+    if response.status_code == 200:
+        data = response.json()
+        return data
+    else:
+        return None
 
 # Create your views here.
 def home(request):
-    if 'city name' in requests.POST:
-        city = request.POST['city']
+    if request.method == 'POST':
+        city = request.POST['cityName']
+    else: 
+        city='indore'
+
+    # Fetch weather data
+    weather_data = get_weather_data(city)
+
+    if weather_data:
+        description = weather_data['weather'][0]['description']
+        icon = weather_data['weather'][0]['icon']
+        temp = weather_data['main']['temp']
+        day = datetime.datetime.today()
+        return render(request, 'result.html', {'description': description, 'icon': icon, 'temp': temp, 'day': day, 'city': city})
     else:
-        city = 'indore'
+        error_message = f"Failed to fetch weather data for {city}. Please try again later."
+        return render(request, 'result.html', {'error_message': error_message})
 
 
-    url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid=6a8c2720970c1dd78fb680438474bc0d'
-    PARAMS = {'units':'metric'}
-    data =request.get(url,PARAMS).json()
-    description =data['weather'][0]['description']
-    icon=data['weather'][0]['icon']
-    temp=data['main']['temp']
-    day = datetime.data.today()
-    return render(request,'weatherapp/index.html',{'description':description,'icon':icon,'temp':temp,'day':day,'city':city})
-   
+def page(request):
+    return render(request, 'index.html')
